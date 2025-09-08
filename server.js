c40a1e707e9f75e0b1d851352cca80e4000bf68f
SabirMalik006 <sabiramir892@gmail.com>
2025-09-08 20:51:34 +0500

    .

--- server.js
+++ server.js
@@ -9,16 +9,23 @@ connectDB();
 
 const app = express();
 
+
 app.use(cors({
-  origin: "*", 
-  credentials: true,              
+  origin: [
+    "http://localhost:3000", 
+    "https://todo-frontend-nextjs-ci89.vercel.app" 
+  ],
+  credentials: true, 
+  methods: ["GET","POST","PUT","DELETE","OPTIONS"]
 }));
 
 app.use(express.json());
 app.use(cookieParser());
 
+
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/todo", require("./routes/todoRoutes"));
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
