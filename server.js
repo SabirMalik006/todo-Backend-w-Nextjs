67690bcae372a5e3dff19457e0afed2dc5d24996
SabirMalik006 <sabiramir892@gmail.com>
2025-09-09 19:00:32 +0500

    .

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:3000",
-    "https://todo-frontend-nextjs-ci89.vercel.app"
+    "https://todo-frontend-nextjs-47j4p7jfg-sabir-maliks-projects.vercel.app/"
   ],
   credentials: true,
   methods: ["GET","POST","PUT","DELETE","OPTIONS"]
